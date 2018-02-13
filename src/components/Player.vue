4a484771bdd0802fe82fec3a7d67eab50325903f
FcoJacob <FcoJacob@users.noreply.github.com>
2018-02-13 20:12:17 +0000

    No mdifica el css

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -1,34 +1,28 @@
 <template>
-  <div class="player" :style="{left: left+'%'}">
-    <div id="x">{{ x }}</div>
-    <div id="y">{{ y }}</div>
-  </div>
+  <div class="player" :style="{left: left+'%'}"></div>
 </template>
 
 <script>
 export default {
   name: 'player',
-  data: function () {
+  data () {
     return {
-      left: 44,
-      x: '',
-      y: ''
+      left: 44
     }
   },
   mounted: function () {
-    this.moveMouse()
+    this.movePlayer()
   },
   methods: {
-    moveMouse: function (event) {
-      event = event || window.event
-      this.x = window.event.clientX
-      this.y = window.event.clientY
-    },
-    keyDownHandler: function (e) {
-      if (e.keyCode === 37) {
-        this.left -= 1
-      } else if (e.keyCode === 39) {
-        this.left += 1
+    movePlayer: function () {
+      window.onkeydown = function (event) {
+        if (event.keyCode === 37) {
+          this.left--
+          console.log(event.code)
+        } else if (event.keyCode === 39) {
+          this.left++
+          console.log(event.code)
+        }
       }
     }
   }
@@ -36,5 +30,5 @@ export default {
 </script>
 
 <style scoped>
-  .player{bottom: calc(48px - 12.5px);left: 50%;width: 150px;height: 25px;position: relative;background-image: url("../assets/player.png");background-size: cover;position: absolute;}
+  .player{bottom: calc(48px - 12.5px);width: 150px;height: 25px;position: relative;background-image: url("../assets/player.png");background-size: cover;position: absolute;}
 </style>
